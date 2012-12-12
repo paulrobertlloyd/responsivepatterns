0cb4030ba6752dc3594506feba5882f121c1e6ec
Josh Emerson <josh.j.emerson@gmail.com>
2012-12-12 17:36:07 +0000

    Gallery 1 functionality added

---
+++ _js/image-gallery-1.js
@@ -0,0 +1,25 @@
+var addEvent=function(){return document.addEventListener?function(a,c,d){if(a&&a.nodeName||a===window)a.addEventListener(c,d,!1);else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}:function(a,c,d){if(a&&a.nodeName||a===window)a.attachEvent("on"+c,function(){return d.call(a,window.event)});else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}}();
+
+if (document.querySelector) {
+    var gallery = document.querySelector('.gallery');
+    if (gallery) {
+        gallery.className += ' is-enhanced';
+        var links = gallery.querySelectorAll('figure .gallery-link');        
+        addEvent(links, 'click', function(e) {
+            updateSlide(this);
+            e.preventDefault();
+        });
+        var fullFig = document.createElement('figure'),
+            fullImg = fullFig.appendChild(document.createElement('img')),
+            fullCaption = fullFig.appendChild(document.createElement('figcaption'));
+        gallery.parentNode.insertBefore(fullFig, gallery);
+        
+        function updateSlide(el) {
+            console.log(el.parentNode);
+            fullImg.src = el.href;
+            fullCaption.innerHTML = el.parentNode.querySelector('figcaption').innerHTML;
+        }
+        updateSlide(links[0]);
+        
+    }
+}
